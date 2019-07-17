87093d85575d84583f54a40497498e543f859fc2
haruhiko yamano <okihurahyamano1005@gmail.com>
2019-07-17 23:17:27 +0900

    Script Testプロジェクト　初めてのコミット
    C#スクリプトの課題について作成したプロジェクトです

---
+++ Assets/Boss.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void defense(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+    }
+
+    public int magic()
+    {
+        int sparemp = mp -= 5;
+
+        Debug.Log("魔法攻撃を行った。残りMPは" + sparemp);
+
+        if (sparemp < 5)
+        {
+            Debug.Log("魔法を使うことができない");
+        }
+
+        return sparemp;
+        
+
+
+
+        
+    }
+    
+}
+public class Boss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss lastboss = new boss();
+
+        lastboss.Attack();
+        lastboss.defense(3);
+
+        for (int a = 0; a < 10; a++)
+        {
+            lastboss.magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+       
+    }
+}
---
+++ Assets/array.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class array : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] point = { 50, 90, 100, 281, 96 };
+
+            for (int i = 0; i < point.Length; i++)
+        {
+            Debug.Log(point[i]);
+        }
+
+            for(int j = 5; j > 0; j--)
+        {
+            Debug.Log(point[j]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
